7d6329a995198331a7cd6d224f5ac9ab4ba46649
Sanjay Khatri <sanzaikc@gmail.com>
2020-07-27 21:27:36 +0545

    nav bug

--- src/main.js
+++ src/main.js
@@ -26,28 +26,20 @@ const router = new VueRouter({
 
 // Navigation Guard 
 router.beforeEach((to, from, next) => {
-  // this route requires auth, check if loggedIn 
-  // if not, redirect to login 
-  if(to.matched.some(record => record.meta.requiresAuth)){
-    if(!store.getters.loggedIn){
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!store.getters.loggedIn()) {
       next({
-        name: 'login',
-        query: { redirect: to.fullPath }
-      });
-    }else{
-      next();
+        name: 'login'
+      })
+    } else {
+      next()
     }
-  }else if(to.matched.some(record => record.meta.requiresVisitor)){
-    if(store.getters.loggedIn){
-      next({
-        name: 'home',
-        query: { redirect: to.fullPath }
-      });
-    }else{
-       next();
-    }
+  } else {
+    next() // make sure to always call next()!
   }
-});
+})
 
 
 new Vue({
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,35 @@ import Login from './components/Auth/Login.vue';
 import Register from './components/Auth/Register.vue';
 import Logout from './components/Auth/Logout.vue';
 import Home from './components/Home.vue';
+import Welcome from './components/Welcome.vue';
 
 export const routes = [
-    { path: '', component: Home, name: 'home', meta: { requiresAuth: true } },
-    { path: '/login', component: Login, name: 'login', meta: { requiresVisitor: true } },
-    { path: '/register', component: Register, name: 'register', meta: { requiresVisitor: true } },
-    { path: '/logout', component: Logout, name: 'logout' },
+    { 
+        path: '', 
+        component: Welcome, 
+        name: 'welcome', 
+    },
+    { 
+        path: '/home', 
+        component: Home, 
+        name: 'home', 
+        meta: { requiresAuth: true } 
+    },
+    { 
+        path: '/login', 
+        component: Login, 
+        name: 'login', 
+        meta: { requiresVisitor: true } 
+    },
+    { 
+        path: '/register', 
+        component: Register, 
+        name: 'register',
+         meta: { requiresVisitor: true } 
+        },
+    { 
+        path: '/logout', 
+        component: Logout, 
+        name: 'logout' 
+    },
 ];
