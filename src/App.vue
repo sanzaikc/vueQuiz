331a8cb8d2fa8ad14fe12cbf50e2f71e11aa636d
JemyJenesh <xemejenesh@gmail.com>
2020-07-28 20:12:16 +0545

    auth redirections
    token check on app load and  role based redirection

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="container bg-light">
-    <navbar></navbar>
-    <transition name="slide-fade" mode="out-in">
-      <router-view></router-view>
-    </transition>
+    <div v-if="isLoading">Loading...</div>
+    <div v-else>
+      <navbar></navbar>
+      <transition name="slide-fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -14,6 +17,16 @@ export default {
   name: "App",
   components: {
     Navbar,
+  },
+  data() {
+    return {
+      isLoading: true,
+    };
+  },
+  mounted() {
+    this.$store.dispatch("restoreToken").finally(() => {
+      this.isLoading = false;
+    });
   },
 };
 </script>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,89 +1,115 @@
-import axios from 'axios'
+import axios from "axios";
 
 const state = {
-    token: localStorage.getItem('accessToken') || null, 
-    currentUser: localStorage.getItem('currentUser') || null,
+	token: localStorage.getItem("accessToken") || null,
+	currentUser: localStorage.getItem("currentUser") || null,
 };
 const mutations = {
-    retrieveToken: (state, token) => {
-        state.token = token;
-        localStorage.setItem('accessToken', token);
-    },
-    destroyToken: (state) => {
-        state.token = null;
-        localStorage.removeItem('accessToken');
-    },
-    setCurrentUser: (state, user) => {
-        state.currentUser = user;
-        localStorage.setItem('currentUser', JSON.stringify(user));
-    },
-    removeUser: (state) => {
-        state.currentUser = null;
-        localStorage.removeItem('currentUser');
-    }
+	retrieveToken: (state, token) => {
+		state.token = token;
+		localStorage.setItem("accessToken", token);
+	},
+	destroyToken: (state) => {
+		state.token = null;
+		localStorage.removeItem("accessToken");
+	},
+	setCurrentUser: (state, user) => {
+		state.currentUser = user;
+		localStorage.setItem("currentUser", JSON.stringify(user));
+	},
+	removeUser: (state) => {
+		state.currentUser = null;
+		localStorage.removeItem("currentUser");
+	},
 };
 const actions = {
-    login: ({ commit }, credentials) => {
-        return new Promise((resolve, reject)=>{
-            axios.post('/login', credentials)
-            .then(res => {
-                let token = res.data.token;
-                commit('retrieveToken', token);
+	restoreToken: (context) => {
+		let token = localStorage.getItem("accessToken");
+		console.log(token);
+		if (token) {
+			axios.defaults.headers.common["Authorization"] = "Bearer " + token;
+			return new Promise((resolve, reject) => {
+				axios
+					.get("/users/me")
+					.then((res) => {
+						let user = res.data.user;
+						context.commit("setCurrentUser", user);
 
-                let user = res.data.user;
-                commit('setCurrentUser', user);
+						resolve(res);
+					})
+					.catch((error) => {
+						context.commit("destroyToken");
+						context.commit("removeUser");
+						reject(error);
+					});
+			});
+		}
+	},
+	login: ({ commit }, credentials) => {
+		return new Promise((resolve, reject) => {
+			axios
+				.post("/login", credentials)
+				.then((res) => {
+					let token = res.data.token;
+					commit("retrieveToken", token);
 
-                resolve(res);
-            })
-            .catch(error => {
-                console.log(error.response.data);
-                reject(error);
-            });
-        });
-    },
-    register: (context, credentials) => {
-        return new Promise((resolve, reject)=>{
-            axios.post('/register', credentials)
-            .then(res => {
-                resolve(res);
-            })
-            .catch(error => {
-                console.log(error.response.data);
-                reject(error);
-            });
-        });
-    },
-    logout: (context) => {
-        axios.defaults.headers.common['Authorization'] = 'Bearer ' + context.state.token;
-        if(context.getters.loggedIn) {
-            return new Promise((resolve, reject)=>{
-                axios.post('/logout')
-                .then(res => {
-                    context.commit('destroyToken');
-                    context.commit('removeUser');
-                    resolve(res);
-                })
-                .catch(error => {
-                    context.commit('destroyToken');
-                    context.commit('removeUser');
-                    reject(error);
-                });
-            });
-        }   
-    }
+					let user = res.data.user;
+					commit("setCurrentUser", user);
+
+					resolve(res);
+				})
+				.catch((error) => {
+					console.log(error.response.data);
+					reject(error);
+				});
+		});
+	},
+	register: (context, credentials) => {
+		return new Promise((resolve, reject) => {
+			axios
+				.post("/register", credentials)
+				.then((res) => {
+					resolve(res);
+				})
+				.catch((error) => {
+					console.log(error.response.data);
+					reject(error);
+				});
+		});
+	},
+	logout: (context) => {
+		axios.defaults.headers.common["Authorization"] =
+			"Bearer " + context.state.token;
+		if (context.getters.loggedIn) {
+			return new Promise((resolve, reject) => {
+				axios
+					.post("/logout")
+					.then((res) => {
+						context.commit("destroyToken");
+						context.commit("removeUser");
+						resolve(res);
+					})
+					.catch((error) => {
+						context.commit("destroyToken");
+						context.commit("removeUser");
+						reject(error);
+					});
+			});
+		}
+	},
 };
 const getters = {
-    loggedIn: state => {
-        return state.token != null;
-    },
-    currentUser: state => {
-        return state.currentUser;
-    }
+	loggedIn: (state) => {
+		return state.token != null;
+	},
+	currentUser: (state) => {
+		return state.currentUser;
+	},
 };
 
 export default {
-    state,
-    mutations,
-    actions,
-    getters
+	state,
+	mutations,
+	actions,
+	getters,
 };
