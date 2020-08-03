728eee4da6056e357790fa9f212ad1df80f82f09
JemyJenesh <xemejenesh@gmail.com>
2020-08-03 19:37:49 +0545

    nav, scroll and key fixes

--- src/pages/admin/Categories.vue
+++ src/pages/admin/Categories.vue
@@ -1,103 +1,121 @@
 <template>
-    <div>
-        <div v-if="isLoading">Loading</div>
-        <div v-else>
-            <div class="d-flex justify-content-between align-items-center">
-                <h2>Categories</h2>
-                <div class="ml-auto d-flex justify-content-between align-items-center">
-                    <transition name="slide-fade" mode="out-in"> 
-                        <div  v-if="show" class="d-flex align-items-center">
-                            <input v-model="category" type="text" class="form-control" placeholder="Category Name">
-                            <button class="btn btn-outline-primary ml-2" @click="createCategory" :disabled="isBusy">
-                                {{ isBusy ? '' : 'Create' }} 
-                                <b-spinner v-if="isBusy" small></b-spinner>
-                            </button>
-                        </div>
-                    </transition>
-                    <button v-text="show ? 'Cancel' : 'Add new'" class="btn btn-outline-primary ml-3" @click="show=!show" ></button>
-                </div>
-            </div>
-            <hr>
-            <transition-group name="slide-fade" class="row" mode="out-in">
-                <div v-for="(category, key) in categories" :key="key" class="m-2">    
-                        <category :category="category" @onDelete="deleteCategory" @onUpdate="updateCategory"></category>        
-                </div>
-            </transition-group>   
-        </div>
-    </div>
+	<div>
+		<div v-if="isLoading">Loading</div>
+		<div v-else>
+			<div class="d-flex justify-content-between align-items-center">
+				<h2>Categories</h2>
+				<div class="ml-auto d-flex justify-content-between align-items-center">
+					<transition name="slide-fade" mode="out-in">
+						<div v-if="show" class="d-flex align-items-center">
+							<input
+								v-model="category"
+								type="text"
+								class="form-control"
+								placeholder="Category Name"
+							/>
+							<button
+								class="btn btn-outline-primary ml-2"
+								@click="createCategory"
+								:disabled="isBusy"
+							>
+								{{ isBusy ? "" : "Create" }}
+								<b-spinner v-if="isBusy" small></b-spinner>
+							</button>
+						</div>
+					</transition>
+					<button
+						v-text="show ? 'Cancel' : 'Add new'"
+						class="btn btn-outline-primary ml-3"
+						@click="show = !show"
+					></button>
+				</div>
+			</div>
+			<hr />
+			<transition-group name="slide-fade" class="row" mode="out-in">
+				<div v-for="category in categories" :key="category.id" class="m-2">
+					<category
+						:category="category"
+						@onDelete="deleteCategory"
+						@onUpdate="updateCategory"
+					></category>
+				</div>
+			</transition-group>
+		</div>
+	</div>
 </template>
 
 <script>
-    import Category from '../../components/Category.vue' ;
-    import {mapState} from 'vuex';
+import Category from "../../components/Category.vue";
+import { mapState } from "vuex";
 
-    export default {
-        data(){
-            return{
-                show: false,
-                isLoading: true,
-                isBusy: false,
-                category: ''
-            }
-        },
-        mounted(){
-            this.$store.dispatch('retrieveCategories').finally(()=>{
-                this.isLoading = false;
-            });
-        },
-        computed: {
-            ...mapState({
-                'categories': state => state.categories.categoryList
-            }),
-        },
-        methods: {
-            createCategory(){
-                this.isBusy = true;
-                this.$store.dispatch('createCategory', {
-                    name: this.category
-                })
-                .then((res)=> {
-                    this.category = '';
-                    this.show = false;
-                    this.isBusy = false;
-                    this.$toasted.show("Category '"+ res.name + "' added!");
-                })
-                .catch(error => {
-                    this.isBusy = false;
-                    this.$toasted.show(error , {
-                        theme: 'bubble'
-                    });
-                });
-            },
-            deleteCategory(id){  
-                this.$store.dispatch('deleteCategory', id)
-                    .then(res=> {
-                        this.$toasted.show("Category '" + res.name + "' was deleted!", {
-                            theme: 'bubble',
-                        });
-                    })
-                    .catch(error => {
-                        this.$toasted.show(error, {
-                            theme: 'bubble'
-                        });
-                    });
-            },
-            updateCategory(id, category){
-                this.$store.dispatch('updateCategory', { id, category})
-                    .then(res => {
-                        this.$toasted.show("Category renamed as '"+ res.name + "'");
-                    })
-                    .catch(error => {
-                        this.$toasted.show(error);
-                    });
-            }
-        },
-        components: {
-            Category,
-        }
-    }
+export default {
+	data() {
+		return {
+			show: false,
+			isLoading: true,
+			isBusy: false,
+			category: "",
+		};
+	},
+	mounted() {
+		this.$store.dispatch("retrieveCategories").finally(() => {
+			this.isLoading = false;
+		});
+	},
+	computed: {
+		...mapState({
+			categories: (state) => state.categories.categoryList,
+		}),
+	},
+	methods: {
+		createCategory() {
+			this.isBusy = true;
+			this.$store
+				.dispatch("createCategory", {
+					name: this.category,
+				})
+				.then((res) => {
+					this.category = "";
+					this.show = false;
+					this.isBusy = false;
+					this.$toasted.show("Category '" + res.name + "' added!");
+				})
+				.catch((error) => {
+					this.isBusy = false;
+					this.$toasted.show(error, {
+						theme: "bubble",
+					});
+				});
+		},
+		deleteCategory(id) {
+			this.$store
+				.dispatch("deleteCategory", id)
+				.then((res) => {
+					this.$toasted.show("Category '" + res.name + "' was deleted!", {
+						theme: "bubble",
+					});
+				})
+				.catch((error) => {
+					this.$toasted.show(error, {
+						theme: "bubble",
+					});
+				});
+		},
+		updateCategory(id, category) {
+			this.$store
+				.dispatch("updateCategory", { id, category })
+				.then((res) => {
+					this.$toasted.show("Category renamed as '" + res.name + "'");
+				})
+				.catch((error) => {
+					this.$toasted.show(error);
+				});
+		},
+	},
+	components: {
+		Category,
+	},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/pages/host/Host.vue
+++ src/pages/host/Host.vue
@@ -1,42 +1,68 @@
 <template>
-  <div>
-    <div class="row vh-100">
-      <div class="col-md-2">
-        <b-nav vertical class="w-100 text py-3">
-          <router-link :to="{ name: 'host.home' }" tag="b-nav-item" active-class="activeTab">
-            <b-icon icon="house" variant="dark" @click="edit=false" class="mr-2"></b-icon>Home
-          </router-link>
-          <router-link :to="{ name: 'host.quiz' }" tag="b-nav-item" active-class="activeTab">
-            <b-icon icon="book" variant="dark" @click="edit=false" class="mr-2"></b-icon>Quizzes
-          </router-link>
-          <router-link :to="{ name: 'host' }" tag="b-nav-item">
-            <b-icon icon="question" variant="dark" @click="edit=false" class="mr-2"></b-icon>Questions
-          </router-link>
-        </b-nav>
-      </div>
-      <div class="col-md-10 pl-4 py-3">
-        <transition name="slide-fade" mode="out-in">
-            <router-view></router-view>
-        </transition>
-      </div>
-    </div>
-  </div>
+	<div>
+		<div class="row h-100">
+			<div class="col-md-2">
+				<b-nav vertical class="w-100 text py-3">
+					<router-link
+						:to="{ name: 'host.home' }"
+						tag="b-nav-item"
+						active-class="activeTab"
+					>
+						<b-icon
+							icon="house"
+							variant="dark"
+							@click="edit = false"
+							class="mr-2"
+						></b-icon
+						>Home
+					</router-link>
+					<router-link
+						:to="{ name: 'host.quiz' }"
+						tag="b-nav-item"
+						active-class="activeTab"
+					>
+						<b-icon
+							icon="book"
+							variant="dark"
+							@click="edit = false"
+							class="mr-2"
+						></b-icon
+						>Quizzes
+					</router-link>
+					<router-link :to="{ name: 'host' }" tag="b-nav-item">
+						<b-icon
+							icon="question"
+							variant="dark"
+							@click="edit = false"
+							class="mr-2"
+						></b-icon
+						>Questions
+					</router-link>
+				</b-nav>
+			</div>
+			<div class="col-md-10 pl-4 py-3">
+				<transition name="slide-fade" mode="out-in">
+					<router-view></router-view>
+				</transition>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  mounted() {
-    document.title = "Host";
-  },
+	mounted() {
+		document.title = "Host";
+	},
 };
 </script>
 
 <style scoped>
 .activeTab,
 a {
-  background-color: #dbf3fc;
-  border-radius: 2rem;
-  font-size: 1rem;
-  font-weight: bold;
+	background-color: #dbf3fc;
+	border-radius: 2rem;
+	font-size: 1rem;
+	font-weight: bold;
 }
 </style>
