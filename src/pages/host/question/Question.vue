138135e6806d089a5a4069dc5c37a9561d6dec83
Sanjay Khatri <sanzaikc@gmail.com>
2020-08-03 18:22:24 +0545

    questionCard added

--- src/pages/host/question/Question.vue
+++ src/pages/host/question/Question.vue
@@ -1,11 +1,16 @@
 <template>
     <div>
         <h2>Questions</h2>
+        <hr>
+        <question-card></question-card>
     </div>
 </template>
 
 <script>
+import QuestionCard from '../../../components/QuestionCard.vue'
 export default {
-    
+    components: {
+        QuestionCard,
+    }
 }
 </script>
