3e3ef326a9b0282b963b3e899dee748fb0c6882a
JemyJenesh <xemejenesh@gmail.com>
2020-08-05 09:10:34 +0545

    question form data fix

--- src/store/modules/questions.js
+++ src/store/modules/questions.js
@@ -1,34 +1,42 @@
-import axios from 'axios';
-import store from '../store'
+import axios from "axios";
+import store from "../store";
 
 const state = {
-    questionList : [],
+	questionList: [],
 };
 const mutations = {};
 const actions = {
-    retriveQuestions: ()=>{},
-    createQuestion: (context, question)=>{ 
-        let questionData = new FormData();
-        questionData.append('body', question.body);
-        questionData.append('category_id', question.category_id);
-        questionData.append('options', question.options);
-        questionData.append('image', question.image);
-        
-        axios.defaults.headers.common["Authorization"] = "Bearer " + store.state.auth.token;
-        axios.post('/questions', questionData, {
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            }
-        })
-    },
-    removeQuestion: ()=>{},
-    updateQuestion: ()=>{},
+	retriveQuestions: () => {},
+	createQuestion: (context, question) => {
+		let questionData = new FormData();
+		questionData.append("body", question.body);
+		questionData.append("category_id", question.category_id);
+
+		for (var i = 0; i < question.options.length; i++) {
+			questionData.append(`options[${i}]['body']`, question.options[i].body);
+			questionData.append(
+				`options[${i}]['correct']`,
+				question.options[i].correct
+			);
+		}
+		questionData.append("image", question.image);
+
+		axios.defaults.headers.common["Authorization"] =
+			"Bearer " + store.state.auth.token;
+		axios.post("/questions", questionData, {
+			headers: {
+				"Content-Type": "multipart/form-data",
+			},
+		});
+	},
+	removeQuestion: () => {},
+	updateQuestion: () => {},
 };
 const getters = {};
 
 export default {
-    state,
-    mutations,
-    actions,
-    getters
-}
+	state,
+	mutations,
+	actions,
+	getters,
+};
