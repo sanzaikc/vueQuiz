9c44e18d3205547b9a790293ba94193d4f003f2a
Sanjay Khatri <sanzaikc@gmail.com>
2020-08-10 17:10:33 +0545

    animatedCounter added

---
+++ src/components/AnimatedCounter.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h1 class="count">{{ animatedNumber }}</h1 >
+    </div>
+</template>
+
+<script>
+import gsap from 'gsap';
+export default {
+    mounted(){
+        this.number = this.num;
+    },
+    props:{
+        num: { type: Number }
+    },
+    data(){
+        return{
+            number: 0,
+            tweenedNumber: 0
+        }
+    },
+    computed: {
+        animatedNumber: function() {
+            return this.tweenedNumber.toFixed(0);
+        }
+    },
+    watch: {
+        number: function(newValue) {
+            gsap.to(this.$data, { duration: 1.5, tweenedNumber: newValue });
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .count{
+        font-size : 10rem;
+        color: lightsteelblue;
+    }
+</style>
--- src/pages/host/Home.vue
+++ src/pages/host/Home.vue
@@ -2,27 +2,21 @@
     <div>
         <div v-if="disabled"> You are not approved yet!</div>
         <div v-else class="d-flex justify-content-around">
-            <div class="col-md-4 border-right">
-                <div class="d-flex justify-content-center align-items-center border border-info rounded-pill">
-                    <h1 class="display-1 text-info">16</h1>
-                    <h5 class="ml-3">Quizes</h5>
+                <div class="col-md-6 d-flex justify-content-center align-items-center rounded-pill">
+                    <animated-counter :num="10"></animated-counter>
+                    <h3 class="ml-3 text-secondary">Quizes</h3>
                 </div>
-            </div>
-            <div class="col-md-2 d-flex align-items-center">
-                <h3 class="text-center">You have created</h3>
-            </div>
-            <div class="col-md-4 border-left">
-                <div class="d-flex justify-content-center align-items-center border border-info rounded-pill">
-                    <h1 class="display-1 text-info">101</h1>
-                    <h5 class="ml-3">Questions</h5>
+                <div class="col-md-6 d-flex justify-content-center align-items-center rounded-pill">
+                    <animated-counter :num="100"></animated-counter>
+                    <h3 class="ml-3 text-secondary">Questions</h3>
                 </div>
-            </div>
         </div>
     </div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import AnimatedCounter from '../../components/AnimatedCounter.vue'
 export default {
     mounted(){
         document.title = "Home";
@@ -31,6 +25,9 @@ export default {
         ...mapState({
             'disabled': state => state.auth.currentUser.is_disabled,
         })
+    },
+    components: {
+        AnimatedCounter,
     }
 }
 </script>
